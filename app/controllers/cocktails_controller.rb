6b5b770e339b27863d1075c9d3c0188c2f3043c9
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-18 17:02:28 +0000

    Removed ingredients route, controller and views. Adjusted controllers, views, and routes to nest doses in cocktails.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,15 +8,13 @@ class CocktailsController < ApplicationController
 
   # GET /cocktails/1 or /cocktails/1.json
   def show
+    @doses = Dose.where(cocktail_id: params[:id])
+    # raise
   end
 
   # GET /cocktails/new
   def new
     @cocktail = Cocktail.new
-  end
-
-  # GET /cocktails/1/edit
-  def edit
   end
 
   # POST /cocktails or /cocktails.json
@@ -31,28 +29,6 @@ class CocktailsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @cocktail.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /cocktails/1 or /cocktails/1.json
-  def update
-    respond_to do |format|
-      if @cocktail.update(cocktail_params)
-        format.html { redirect_to @cocktail, notice: "Cocktail was successfully updated." }
-        format.json { render :show, status: :ok, location: @cocktail }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /cocktails/1 or /cocktails/1.json
-  def destroy
-    @cocktail.destroy
-    respond_to do |format|
-      format.html { redirect_to cocktails_url, notice: "Cocktail was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,52 +1,24 @@
 class DosesController < ApplicationController
   before_action :set_dose, only: %i[ show edit update destroy ]
-
-  # GET /doses or /doses.json
-  def index
-    @doses = Dose.all
-  end
-
-  # GET /doses/1 or /doses/1.json
-  def show
-  end
-
+  
   # GET /doses/new
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
-
-  # GET /doses/1/edit
-  def edit
-  end
-
+  
   # POST /doses or /doses.json
   def create
     @dose = Dose.new(dose_params)
-
-    respond_to do |format|
-      if @dose.save
-        format.html { redirect_to @dose, notice: "Dose was successfully created." }
-        format.json { render :show, status: :created, location: @dose }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @dose.errors, status: :unprocessable_entity }
-      end
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail), notice: "Dose was successfully created."
+    else
+      render :new
     end
   end
-
-  # PATCH/PUT /doses/1 or /doses/1.json
-  def update
-    respond_to do |format|
-      if @dose.update(dose_params)
-        format.html { redirect_to @dose, notice: "Dose was successfully updated." }
-        format.json { render :show, status: :ok, location: @dose }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @dose.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
+  
   # DELETE /doses/1 or /doses/1.json
   def destroy
     @dose.destroy
@@ -55,15 +27,15 @@ class DosesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dose
-      @dose = Dose.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def dose_params
-      params.require(:dose).permit(:description, :cocktail_id_id, :ingredient_id_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+  
+  # Only allow a list of trusted parameters through.
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
