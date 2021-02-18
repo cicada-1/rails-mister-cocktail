64e7f7fbb81e24c71b08bb94bc398e586783550f
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-18 14:42:14 +0000

    Recreated and ran migration for doses to fix error. Created associations and validations for Cocktail, Ingredient, and Dose.

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail_id
-  belongs_to :ingredient_id
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient, message: "should have only one cocktail/ingredient pairing" }
 end
